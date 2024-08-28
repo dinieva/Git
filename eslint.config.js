08d8823d5757731e085cc9b2aad37db88640349a
Dinieva Rezeda <dinievarezeda@mail.ru>
2024-08-28 23:58:02 +0300

    HW2 add rules in eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    rules: {
+      'no-unused-vars': 'error',
+      'object-shorthand': ['error', 'always'],
+      'curly': 'error',
+      'no-redeclare': 'error',
+      'quotes': ['error', 'single'],
+      'keyword-spacing': ['error', { 'before': true, 'after': true }],
+      'eqeqeq':  'error',
+      'no-unreachable': 'error'
+    }
+  }
+];
